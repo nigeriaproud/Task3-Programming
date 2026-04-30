0fb9d3985a1e04e99b06a9dab25bb1b376f77108
chukwu <183565551+nigeriaproud@users.noreply.github.com>
2026-04-30 20:44:46 +0900

    Add program to calculate area of a triangle

---
+++ kadai5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    double w, h;
+    scanf("%lf", &w);
+    scanf("%lf", &h);
+    //scanf("%d", &c);
+    
+    printf("%lf\n",(w * h) / 2);
+    return 0;
+}
