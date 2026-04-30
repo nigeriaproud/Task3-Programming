9bef6acdeb696021eefd8a45ec459d38ed303319
chukwu <183565551+nigeriaproud@users.noreply.github.com>
2026-04-30 13:37:22 +0900

    Create kadai3.c

---
+++ kadai3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int a, b, c;
+    int apple=100, orange=88, strawberry=398 ;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    printf("%d\n",(apple*a)+(orange*b)+(strawberry*c));
+    return 0;
+}
